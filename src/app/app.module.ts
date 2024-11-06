bf9b43c5823bf19a7407fd350793986f35894688
m-fuentesr <maj.fuentes@duocuc.cl>
2024-11-06 02:05:01 -0300

    Detalles firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { StorageService } from './services/dbstorage.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { IonicStorageModule } from '@ionic/storage-angular';
+/* import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { environment } from '../environments/environment'; */
+import { StorageService } from './services/dbstorage.service';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicStorageModule.forRoot(),IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicStorageModule.forRoot(),
+    IonicModule.forRoot(),
+    AppRoutingModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, StorageService],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA8NlQWkaKqE24HT-EdwjnplRz3NFCtKoU",
+    authDomain: "registrapp-f96cb.firebaseapp.com",
+    projectId: "registrapp-f96cb",
+    storageBucket: "registrapp-f96cb.firebasestorage.app",
+    messagingSenderId: "445281907707",
+    appId: "1:445281907707:web:34d2fabebebd16493d115e",
+    measurementId: "G-PKSCWGRYHK"
+  }
 };
 
 /*
