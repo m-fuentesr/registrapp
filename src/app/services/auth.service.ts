cc59c5d231d46e351a73bc3b0a75dca7d320689a
m-fuentesr <maj.fuentes@duocuc.cl>
2024-11-08 02:51:34 -0300

    Recuperar contraseña funcionando

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AlertController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private afAuth: AngularFireAuth) { }
+  constructor(private afAuth: AngularFireAuth, private alertController: AlertController) { }
 
   // Método para registrar usuarios
   async register(email: string, password: string) {
@@ -37,4 +38,26 @@ export class AuthService {
     await this.afAuth.signOut();
     console.log('Sesión cerrada');
   }
+
+  // Método para enviar el correo de restablecimiento de contraseña
+  async resetPassword(email: string): Promise<void> {
+    try {
+      await this.afAuth.sendPasswordResetEmail(email);
+      this.showAlert('Correo Enviado', 'Revisa tu bandeja de entrada para restablecer tu contraseña.');
+    } catch (error) {
+      console.error('Error al enviar el correo de recuperación de contraseña', error);
+      this.showAlert('Error', 'No se pudo enviar el correo de recuperación. Verifica que el correo sea válido.');
+    }
+  }
+
+  // Método para mostrar alertas
+  async showAlert(header: string, message: string) {
+    const alert = await this.alertController.create({
+      header,
+      message,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+  
 }
