98dbc34b4c3affefa629f5af4b29d5d99d152cb9
zakren1 <zakren1234@gmail.com>
2024-12-04 21:47:06 -0300

    ultimo intento

--- src/app/pages/asignaturas-docente/asignaturas-docente.page.ts
+++ src/app/pages/asignaturas-docente/asignaturas-docente.page.ts
@@ -36,7 +36,6 @@ export class AsignaturasDocentePage implements OnInit {
   ngOnInit() {
     this.initStorage();
     this.route.queryParams.subscribe(params => {
-      console.log('Parametros de la ruta:', params);
       this.asignaturaId = params['asignaturaId'] || '';
       const seccionNombre = params['seccion'];
       if (this.asignaturaId && seccionNombre) {
@@ -62,10 +61,8 @@ export class AsignaturasDocentePage implements OnInit {
   }
 
   async obtenerSeccionYClases(seccionNombre: string) {
-    const seccionOffline = await this.storage.get('seccionOffline'); // Verifica si hay datos offline guardados
-    console.log('Datos offline:', seccionOffline);
+    const seccionOffline = await this.storage.get('seccionOffline');
     if (navigator.onLine) {
-      // Si hay conexión a Internet, cargar desde Firestore
       this.firestore
         .collection('asignaturas')
         .doc(this.asignaturaId)
@@ -75,30 +72,20 @@ export class AsignaturasDocentePage implements OnInit {
             this.asignaturaNombre = asignatura.nombre;
             this.seccion = asignatura.secciones[seccionNombre];
             this.clasesGeneradas = this.seccion?.clases || [];
-            console.log('Clases generadas desde Firestore:', this.clasesGeneradas);
-
-            // Guardar las clases obtenidas en Ionic Storage para acceso offline
             await this.storage.set('seccionOffline', {
               asignatura: asignatura.nombre,
               clases: this.seccion?.clases,
               nombre: this.seccion?.nombre
             });
-            console.log('Datos guardados en storage:', this.seccion);
             this.obtenerAlumnos();
-          } else {
-            console.error('No se encontró la asignatura o la sección.');
           }
         });
     } else {
-      // Si no hay conexión, cargar desde el almacenamiento local (Offline)
       if (seccionOffline) {
         this.asignaturaNombre = seccionOffline.asignatura;
         this.seccion = seccionOffline;
         this.clasesGeneradas = this.seccion?.clases || [];
-        console.log('Clases generadas desde almacenamiento local:', this.clasesGeneradas);
         this.obtenerAlumnos();
-      } else {
-        console.error('No hay datos de la sección disponibles offline.');
       }
     }
   }
@@ -106,7 +93,6 @@ export class AsignaturasDocentePage implements OnInit {
   async obtenerClasesGeneradas() {
     const status = await Network.getStatus();
     if (status.connected) {
-      // Recuperar desde Firestore
       this.firestore
         .collection('asignaturas')
         .doc(this.asignaturaId)
@@ -115,28 +101,13 @@ export class AsignaturasDocentePage implements OnInit {
           if (asignatura?.secciones?.[this.seccion.nombre]?.clases) {
             this.clasesGeneradas = asignatura.secciones[this.seccion.nombre].clases;
             this.totalClases = this.clasesGeneradas.length;
-          } else {
-            this.clasesGeneradas = [];
-            this.totalClases = 0;
           }
         });
     } else {
-      // Recuperar clases desde almacenamiento local si no hay conexión
-      try {
-        const seccionOffline = await this.storage.get('seccionOffline');
-        if (seccionOffline) {
-          this.clasesGeneradas = seccionOffline.clases || [];
-          this.totalClases = this.clasesGeneradas.length;
-          console.log('Clases generadas desde almacenamiento local:', this.clasesGeneradas);
-        } else {
-          this.clasesGeneradas = [];
-          this.totalClases = 0;
-          console.log('No hay clases almacenadas offline.');
-        }
-      } catch (error) {
-        console.error('Error al obtener clases offline:', error);
-        this.clasesGeneradas = [];
-        this.totalClases = 0;
+      const seccionOffline = await this.storage.get('seccionOffline');
+      if (seccionOffline) {
+        this.clasesGeneradas = seccionOffline.clases || [];
+        this.totalClases = this.clasesGeneradas.length;
       }
     }
   }
@@ -152,7 +123,6 @@ export class AsignaturasDocentePage implements OnInit {
           this.obtenerDetallesAlumnos(alumnosRegistrados).then(alumnosDetallados => {
             this.alumnos = alumnosDetallados;
 
-            // Relacionar asistencia de Firestore
             this.firestore.collection('asistencia').valueChanges().subscribe((asistencias: any[]) => {
               this.alumnos.forEach(alumno => {
                 const asistencia = asistencias.find(a => a.alumnoId === alumno.alumnoId);
@@ -178,6 +148,13 @@ export class AsignaturasDocentePage implements OnInit {
           });
         }
       });
+  }
+
+  onClaseSeleccionada(clase: any) {
+    this.claseSeleccionada = clase;
+    this.qrCodeUrl = clase?.qrCodeUrl || '';
+    console.log('Clase seleccionada:', this.claseSeleccionada);
+    console.log('Código QR cargado:', this.qrCodeUrl);
   }
 
   private async obtenerDetallesAlumnos(alumnosRegistrados: any[]): Promise<any[]> {
@@ -220,53 +197,18 @@ export class AsignaturasDocentePage implements OnInit {
     try {
       this.qrCodeUrl = await QRCode.toDataURL(JSON.stringify(datosClase));
       this.cdr.detectChanges();
-      console.log('Código QR generado:', this.qrCodeUrl);
 
-      // Guardar datos de la clase
       const nuevaClase = { ...datosClase, qrCodeUrl: this.qrCodeUrl };
       this.clasesGeneradas.push(nuevaClase);
 
       if (await Network.getStatus().then(status => !status.connected)) {
-        // Si no hay conexión, guardar la clase offline
-        const clasesOffline = await this.storage.get('clasesOffline') || [];
-        clasesOffline.push(nuevaClase);
-        await this.storage.set('clasesOffline', clasesOffline);
-        console.log('Clase guardada offline:', nuevaClase);
+        this.procesarCodigoqrSinConexion(nuevaClase);
       } else {
-        // Si hay conexión, guardar en Firestore
-        await this.firestore.collection('asignaturas').doc(this.asignaturaId).update({
-          [`secciones.${this.seccion.nombre}.clases`]: this.clasesGeneradas,
-        });
+        this.guardarClaseEnFirestore(nuevaClase);
       }
 
-      await this.mostrarAlerta('QR Generado', `Clase: ${nombreClase}, Fecha: ${fechaActual}`);
     } catch (error) {
       console.error('Error al generar el código QR:', error);
-      await this.mostrarAlerta('Error', 'Hubo un problema al generar el QR. Inténtalo de nuevo.');
-    }
-  }
-
-  async sincronizarDatosOffline() {
-    const status = await Network.getStatus();
-    if (!status.connected) return; // Solo sincronizar si hay conexión
-
-    const clasesOffline = await this.storage.get('clasesOffline') || [];
-    if (clasesOffline.length > 0) {
-      try {
-        const clasesActualizadas = [...this.clasesGeneradas, ...clasesOffline];
-        await this.firestore.collection('asignaturas').doc(this.asignaturaId).update({
-          [`secciones.${this.seccion.nombre}.clases`]: clasesActualizadas,
-        });
-
-        // Eliminar datos locales sincronizados
-        await this.storage.remove('clasesOffline');
-        this.clasesGeneradas = clasesActualizadas;
-        this.totalClases = clasesActualizadas.length;
-
-        console.log('Clases offline sincronizadas con Firestore.');
-      } catch (error) {
-        console.error('Error al sincronizar clases offline:', error);
-      }
     }
   }
 
@@ -277,40 +219,64 @@ export class AsignaturasDocentePage implements OnInit {
     console.log('Clase almacenada offline:', datosClase);
   }
 
-  onClaseSeleccionada(clase: any) {
-    this.claseSeleccionada = clase;
-    this.qrCodeUrl = clase?.qrCodeUrl || '';
-    console.log('Clase seleccionada:', this.claseSeleccionada);
-    console.log('Código QR cargado:', this.qrCodeUrl);
+  async guardarClaseEnFirestore(datosClase: any) {
+    try {
+      await this.firestore.collection('asignaturas').doc(this.asignaturaId).update({
+        [`secciones.${this.seccion.nombre}.clases`]: this.clasesGeneradas,
+      });
+
+      await this.storage.set('seccionOffline', {
+        asignatura: this.asignaturaNombre,
+        clases: this.clasesGeneradas,
+        nombre: this.seccion?.nombre,
+      });
+
+      console.log('Clase guardada en Firestore y Storage.');
+    } catch (error) {
+      console.error('Error al guardar clase en Firestore:', error);
+    }
   }
 
-  async mostrarAlerta(titulo: string, mensaje: string) {
+  async sincronizarDatosOffline() {
+    const status = await Network.getStatus();
+    if (!status.connected) return;
+
+    const clasesOffline = await this.storage.get('clasesOffline') || [];
+    if (clasesOffline.length > 0) {
+      try {
+        const clasesActualizadas = [...this.clasesGeneradas, ...clasesOffline];
+        await this.firestore.collection('asignaturas').doc(this.asignaturaId).update({
+          [`secciones.${this.seccion.nombre}.clases`]: clasesActualizadas,
+        });
+
+        await this.storage.remove('clasesOffline');
+        this.clasesGeneradas = clasesActualizadas;
+        this.totalClases = clasesActualizadas.length;
+        console.log('Clases offline sincronizadas con Firestore.');
+      } catch (error) {
+        console.error('Error al sincronizar clases offline:', error);
+      }
+    }
+  }
+
+  async mostrarConfirmacion(titulo: string, mensaje: string) {
     const alert = await this.alertController.create({
       header: titulo,
       message: mensaje,
-      buttons: ['OK']
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+        },
+        {
+          text: 'Aceptar',
+          handler: () => true,
+        },
+      ],
     });
     await alert.present();
+    const { data } = await alert.onDidDismiss();
+    return data?.action === 'Aceptar';
   }
 
-  async mostrarConfirmacion(titulo: string, mensaje: string): Promise<boolean> {
-    return new Promise(async resolve => {
-      const alert = await this.alertController.create({
-        header: titulo,
-        message: mensaje,
-        buttons: [
-          {
-            text: 'Cancelar',
-            role: 'cancel',
-            handler: () => resolve(false),
-          },
-          {
-            text: 'Aceptar',
-            handler: () => resolve(true),
-          },
-        ],
-      });
-      await alert.present();
-    });
-  }
 }
